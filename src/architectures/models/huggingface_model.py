838c9e9a779fc296d74ba21334740886ee1dd1d2
DimensionSTP <ddang8jh@gmail.com>
2024-05-12 08:10:42 +0900

    feat: only decoder model, peft, quantization

--- src/architectures/models/huggingface_model.py
+++ src/architectures/models/huggingface_model.py
@@ -3,19 +3,61 @@ from typing import Dict
 import torch
 from torch import nn
 
-from transformers import BartForConditionalGeneration
+from transformers import (
+    BitsAndBytesConfig,
+    BartForConditionalGeneration,
+    T5ForConditionalGeneration,
+    AutoModelForCausalLM,
+)
+
+from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model
 
 
 class HuggingFaceModel(nn.Module):
     def __init__(
         self,
         pretrained_model_name: str,
+        quantization_type: str,
+        quantization_config: BitsAndBytesConfig,
+        peft_type: str,
+        peft_config: LoraConfig,
     ) -> None:
         super().__init__()
-        self.model = BartForConditionalGeneration.from_pretrained(
-            pretrained_model_name,
-            output_hidden_states=False,
-        )
+        if quantization_type == "quantization":
+            self.quantization_config = quantization_config
+        elif quantization_type == "origin":
+            self.quantization_config = None
+        else:
+            raise ValueError(f"Invalid quantization type: {quantization_type}.")
+
+        if "bart" in pretrained_model_name:
+            model = BartForConditionalGeneration.from_pretrained(
+                pretrained_model_name,
+                output_hidden_states=False,
+                quantization_config=self.quantization_config,
+            )
+        elif "t5" in pretrained_model_name:
+            model = T5ForConditionalGeneration.from_pretrained(
+                pretrained_model_name,
+                output_hidden_states=False,
+                quantization_config=self.quantization_config,
+            )
+        else:
+            model = AutoModelForCausalLM.from_pretrained(
+                pretrained_model_name,
+                output_hidden_states=False,
+                quantization_config=self.quantization_config,
+            )
+
+        if quantization_type == "quantization":
+            model = prepare_model_for_kbit_training(model)
+
+        if peft_type == "lora":
+            self.model = get_peft_model(model, peft_config)
+        elif peft_type == "origin":
+            self.model = model
+        else:
+            raise ValueError(f"Invalid PEFT type: {peft_type}.")
 
     def forward(
         self,
