9a694495aea7bb46c4dddc0a0b6944f5c6b71715
DimensionSTP <ddang8jh@gmail.com>
2024-05-13 11:47:43 +0900

    chore: fix typo of term

--- src/datasets/huggingface_dataset.py
+++ src/datasets/huggingface_dataset.py
@@ -9,7 +9,7 @@ from torch.utils.data import Dataset
 from transformers import AutoTokenizer
 
 
-class UpStageDialogsDataset(Dataset):
+class UpStageDialoguesDataset(Dataset):
     def __init__(
         self,
         data_path: str,
