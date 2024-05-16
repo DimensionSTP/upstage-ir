e166ea98ae0b327de62d87fbe191770b07296b6c
DimensionSTP <ddang8jh@gmail.com>
2024-05-17 07:26:12 +0900

    feat: BART, T5 instantiate option merged, generate option changed by model type

--- src/architectures/models/huggingface_model.py
+++ src/architectures/models/huggingface_model.py
@@ -4,12 +4,7 @@ import os
 import torch
 from torch import nn
 
-from transformers import (
-    BitsAndBytesConfig,
-    BartForConditionalGeneration,
-    T5ForConditionalGeneration,
-    AutoModelForCausalLM,
-)
+from transformers import BitsAndBytesConfig, AutoModelForSeq2SeqLM, AutoModelForCausalLM
 
 from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model
 
@@ -25,14 +20,25 @@ class HuggingFaceModel(nn.Module):
         peft_config: LoraConfig,
     ) -> None:
         super().__init__()
+        self.pretrained_model_name = pretrained_model_name
         if precision == 32 or precision == "32":
             self.precision = torch.float32
+            self.attn_implementation = None
         elif precision == 16 or precision == "16":
             self.precision = torch.float16
+            if "t5" in self.pretrained_model_name:
+                self.attn_implementation = None
+            else:
+                self.attn_implementation = "flash_attention_2"
         elif precision == "bf16":
             self.precision = torch.bfloat16
+            if "t5" in self.pretrained_model_name:
+                self.attn_implementation = None
+            else:
+                self.attn_implementation = "flash_attention_2"
         else:
             self.precision = "auto"
+            self.attn_implementation = None
 
         if quantization_type == "quantization":
             self.quantization_config = quantization_config
@@ -48,32 +54,22 @@ class HuggingFaceModel(nn.Module):
         else:
             raise ValueError(f"Invalid quantization type: {quantization_type}.")
 
-        if "bart" in pretrained_model_name:
-            model = BartForConditionalGeneration.from_pretrained(
-                pretrained_model_name,
+        if "bart" in self.pretrained_model_name or "t5" in self.pretrained_model_name:
+            model = AutoModelForSeq2SeqLM.from_pretrained(
+                self.pretrained_model_name,
                 output_hidden_states=False,
                 torch_dtype=self.precision,
-                attn_implementation="flash_attention_2",
-                quantization_config=self.quantization_config,
-                device_map=self.device_map,
-                load_in_4bit=self.load_in_4bit,
-            )
-        elif "t5" in pretrained_model_name:
-            model = T5ForConditionalGeneration.from_pretrained(
-                pretrained_model_name,
-                output_hidden_states=False,
-                torch_dtype=self.precision,
-                attn_implementation=None,
+                attn_implementation=self.attn_implementation,
                 quantization_config=self.quantization_config,
                 device_map=self.device_map,
                 load_in_4bit=self.load_in_4bit,
             )
         else:
             model = AutoModelForCausalLM.from_pretrained(
-                pretrained_model_name,
+                self.pretrained_model_name,
                 output_hidden_states=False,
                 torch_dtype=self.precision,
-                attn_implementation="flash_attention_2",
+                attn_implementation=self.attn_implementation,
                 quantization_config=self.quantization_config,
                 device_map=self.device_map,
                 load_in_4bit=self.load_in_4bit,
@@ -106,7 +102,12 @@ class HuggingFaceModel(nn.Module):
         self,
         encoded: Dict[str, torch.Tensor],
         options: Dict[str, Any],
+        target_max_length: int,
     ) -> torch.Tensor:
+        if "bart" in self.pretrained_model_name or "t5" in self.pretrained_model_name:
+            options["max_length"] = target_max_length
+        else:
+            options["max_new_tokens"] = target_max_length
         output = self.model.generate(
             **{
                 **encoded,
