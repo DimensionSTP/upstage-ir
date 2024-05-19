2477ae3eaf9b03c69dfe4526b29933a2f5b44d98
DimensionSTP <ddang8jh@gmail.com>
2024-05-20 04:02:45 +0900

    add: generate from logits

---
+++ decode_predictions.py
@@ -0,0 +1,78 @@
+import os
+import warnings
+
+os.environ["HYDRA_FULL_ERROR"] = "1"
+warnings.filterwarnings("ignore")
+
+import numpy as np
+import pandas as pd
+
+from transformers import AutoTokenizer
+
+import hydra
+from omegaconf import DictConfig
+
+
+@hydra.main(
+    config_path="configs/",
+    config_name="huggingface.yaml",
+)
+def merge_predictions(
+    config: DictConfig,
+) -> None:
+    def generate_prompt(
+        data: str,
+    ) -> str:
+        default_system_prompt = "Summarize the dialogues of two people appropriately."
+        prompt = f"""### Instruction:
+        {default_system_prompt} 
+
+        ### Input:
+        {data.strip()}
+
+        ### Response:
+        """.strip()
+        return prompt
+
+    all_predictions = np.load(f"{config.connected_dir}/preds/{config.pred_name}.npy")
+    generation_df = pd.read_csv(
+        f"{config.connected_dir}/data/{config.submission_file_name}.csv"
+    )
+    generation_df["prompt"] = generation_df["dialogue"].apply(generate_prompt)
+    tokenizer = AutoTokenizer.from_pretrained(
+        config.pretrained_model_name,
+        use_fast=True,
+    )
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+    generation_df["encoded_prompt_length"] = generation_df["prompt"].apply(
+        lambda x: len(
+            tokenizer(
+                x,
+                return_tensors="pt",
+                add_special_tokens=True,
+            )["input_ids"]
+        )
+    )
+    decoded_predictions = []
+    for i in range(len(all_predictions[0])):
+        encoded_prompt_length = generation_df.loc[i, "encoded_prompt_length"]
+        summary_prediction = all_predictions[i, encoded_prompt_length:]
+        decoded_prediction = tokenizer.batch_decode(
+            sequences=summary_prediction,
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=True,
+        )
+        decoded_predictions.append(decoded_prediction)
+    generation_df = pd.read_csv(
+        f"{config.connected_dir}/data/{config.submission_file_name}.csv"
+    )
+    generation_df[config.target_column_name] = decoded_predictions
+    generation_df.to_csv(
+        f"{config.connected_dir}/submissions/from_decoded_predictions/{config.pred_name}.csv",
+        index=False,
+    )
+
+
+if __name__ == "__main__":
+    merge_predictions()
