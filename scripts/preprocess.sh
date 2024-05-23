44b5d564a470360dee7cf4788d3adc3190af42a4
DimensionSTP <ddang8jh@gmail.com>
2024-05-24 01:38:39 +0900

    add: preprocess shell scripts

---
+++ scripts/preprocess.sh
@@ -0,0 +1,8 @@
+python src/preprocessing/add_empty_column.py
+python src/preprocessing/make_corpus.py
+python src/preprocessing/train_tokenizer.py
+python src/preprocessing/merge_tokenizer.py
+python src/preprocessing/merge_model.py
+python src/preprocessing/preprocess_dataset.py mode=train
+python src/preprocessing/preprocess_dataset.py mode=dev
+python src/preprocessing/preprocess_dataset.py mode=test
