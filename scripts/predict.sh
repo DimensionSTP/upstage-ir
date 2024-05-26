c9e06fa4a2185d61dde4ce89d7458201eeeb0365
DimensionSTP <ddang8jh@gmail.com>
2024-05-27 07:58:56 +0900

    fix: syntex error

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -15,51 +15,51 @@ epochs="3 4"
 
 for epoch in $epochs
 do
-python main.py mode=predict \
-    is_preprocessed=$is_preprocessed \
-    is_tuned=$is_tuned \
-    strategy=$strategy \
-    upload_user=$upload_user \
-    model_type=$model_type \
-    quantization_type=$quantization_type \
-    peft_type=$peft_type \
-    data_max_length=$data_max_length \
-    target_max_length=$target_max_length \
-    precision=$precision \
-    batch_size=$batch_size \
-    epoch=$epoch
+    python main.py mode=predict \
+        is_preprocessed=$is_preprocessed \
+        is_tuned=$is_tuned \
+        strategy=$strategy \
+        upload_user=$upload_user \
+        model_type=$model_type \
+        quantization_type=$quantization_type \
+        peft_type=$peft_type \
+        data_max_length=$data_max_length \
+        target_max_length=$target_max_length \
+        precision=$precision \
+        batch_size=$batch_size \
+        epoch=$epoch
 done
 
 for epoch in $epochs
 do
-python merge_predictions.py \
-    is_preprocessed=$is_preprocessed \
-    is_tuned=$is_tuned \
-    strategy=$strategy \
-    upload_user=$upload_user \
-    model_type=$model_type \
-    quantization_type=$quantization_type \
-    peft_type=$peft_type \
-    data_max_length=$data_max_length \
-    target_max_length=$target_max_length \
-    precision=$precision \
-    batch_size=$batch_size \
-    epoch=$epoch
+    python merge_predictions.py \
+        is_preprocessed=$is_preprocessed \
+        is_tuned=$is_tuned \
+        strategy=$strategy \
+        upload_user=$upload_user \
+        model_type=$model_type \
+        quantization_type=$quantization_type \
+        peft_type=$peft_type \
+        data_max_length=$data_max_length \
+        target_max_length=$target_max_length \
+        precision=$precision \
+        batch_size=$batch_size \
+        epoch=$epoch
 done
 
 for epoch in $epochs
 do
-python decode_predictions.py \
-    is_preprocessed=$is_preprocessed \
-    is_tuned=$is_tuned \
-    strategy=$strategy \
-    upload_user=$upload_user \
-    model_type=$model_type \
-    quantization_type=$quantization_type \
-    peft_type=$peft_type \
-    data_max_length=$data_max_length \
-    target_max_length=$target_max_length \
-    precision=$precision \
-    batch_size=$batch_size \
-    epoch=$epoch
+    python decode_predictions.py \
+        is_preprocessed=$is_preprocessed \
+        is_tuned=$is_tuned \
+        strategy=$strategy \
+        upload_user=$upload_user \
+        model_type=$model_type \
+        quantization_type=$quantization_type \
+        peft_type=$peft_type \
+        data_max_length=$data_max_length \
+        target_max_length=$target_max_length \
+        precision=$precision \
+        batch_size=$batch_size \
+        epoch=$epoch
 done
