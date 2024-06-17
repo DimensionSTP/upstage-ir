483e0bf965cde7ffd2396e47202cc16c1b491a27
DimensionSTP <ddang8jh@gmail.com>
2024-06-17 09:34:41 +0900

    feat: adapt all kinds of models

--- src/preprocessing/merge_model.py
+++ src/preprocessing/merge_model.py
@@ -4,7 +4,7 @@ dotenv.load_dotenv(
     override=True,
 )
 
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModel
 
 import hydra
 from omegaconf import DictConfig
@@ -20,7 +20,7 @@ def merge_model(
     tokenizer = AutoTokenizer.from_pretrained(
         f"{config.custom_data_encoder_path}/{config.pretrained_model_name}"
     )
-    model = AutoModelForCausalLM.from_pretrained(config.pretrained_model_name)
+    model = AutoModel.from_pretrained(config.pretrained_model_name)
     if tokenizer.pad_token_id is None:
         tokenizer.pad_token_id = tokenizer.eos_token_id
     model.resize_token_embeddings(len(tokenizer))
