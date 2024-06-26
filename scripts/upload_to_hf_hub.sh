d100059a03b5b29d73e15231c42a302f8759e50c
DimensionSTP <ddang8jh@gmail.com>
2024-06-26 14:09:24 +0900

    add: a script of upload hf hub

---
+++ scripts/upload_to_hf_hub.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+path="src/preprocessing"
+is_tuned="untuned"
+strategy="deepspeed_stage_3_offload"
+upload_user="beomi"
+model_type="OPEN-SOLAR-KO-10.7B"
+quantization_type="origin"
+peft_type="origin"
+data_max_length=508
+target_max_length=260
+precision="bf16"
+batch_size=32
+epoch=30
+
+python $path/upload_to_hf_hub.py \
+    is_preprocessed=$is_preprocessed \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    upload_user=$upload_user \
+    model_type=$model_type \
+    quantization_type=$quantization_type \
+    peft_type=$peft_type \
+    data_max_length=$data_max_length \
+    target_max_length=$target_max_length \
+    precision=$precision \
+    batch_size=$batch_size \
+    epoch=$epoch
