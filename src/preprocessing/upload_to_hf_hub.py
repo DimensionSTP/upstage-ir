5b240b6719ead7efc7ea6e5b9907cbdf97d079b4
DimensionSTP <ddang8jh@gmail.com>
2024-06-26 14:08:34 +0900

    add: upload hf hub module

---
+++ src/preprocessing/upload_to_hf_hub.py
@@ -0,0 +1,32 @@
+import dotenv
+
+dotenv.load_dotenv(
+    override=True,
+)
+
+from huggingface_hub import HfApi, HfFolder
+
+import hydra
+from omegaconf import DictConfig
+
+
+@hydra.main(
+    config_path="../../configs/",
+    config_name="huggingface.yaml",
+)
+def upload_to_hf_hub(
+    config: DictConfig,
+) -> None:
+    save_dir = f"{config.connected_dir}/prepare_upload/{config.pretrained_model_name}/epoch={config.epoch}"
+    api = HfApi()
+    token = HfFolder.get_token()
+    api.upload_folder(
+        folder_path=save_dir,
+        repo_id=f"{config.user_name}/{config.model_type}-scientificQA",
+        repo_type="model",
+        token=token,
+    )
+
+
+if __name__ == "__main__":
+    upload_to_hf_hub()
