4d6b684fba2f63e23c0992f242e044108c94cbbd
DimensionSTP <ddang8jh@gmail.com>
2024-06-29 06:53:58 +0900

    feat: no for loop, once at an execution

--- scripts/prepare_upload.sh
+++ scripts/prepare_upload.sh
@@ -11,21 +11,18 @@ data_max_length=508
 target_max_length=260
 precision="bf16"
 batch_size=32
-epochs="30 32"
+epoch=30
 
-for epoch in $epochs
-do
-    python $path/prepare_upload.py \
-        is_preprocessed=$is_preprocessed \
-        is_tuned=$is_tuned \
-        strategy=$strategy \
-        upload_user=$upload_user \
-        model_type=$model_type \
-        quantization_type=$quantization_type \
-        peft_type=$peft_type \
-        data_max_length=$data_max_length \
-        target_max_length=$target_max_length \
-        precision=$precision \
-        batch_size=$batch_size \
-        epoch=$epoch
-done
+python $path/prepare_upload.py \
+    is_preprocessed=$is_preprocessed \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    upload_user=$upload_user \
+    model_type=$model_type \
+    quantization_type=$quantization_type \
+    peft_type=$peft_type \
+    data_max_length=$data_max_length \
+    target_max_length=$target_max_length \
+    precision=$precision \
+    batch_size=$batch_size \
+    epoch=$epoch
